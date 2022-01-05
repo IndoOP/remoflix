86e7cb95c3bea942904d71ff0b61ef3d10c558ac
IndoOP <96644081+IndoOP@users.noreply.github.com>
2022-01-05 20:57:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ var video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 
 // Pause and play the video, and change the button text
-const myFunction = () => {
-  if (video.paused) {
+btn.addEventListener("click", function(){
+ if (video.paused) {
     video.play();
     btn.innerHTML = "Pause";
   } else {
     video.pause();
     btn.innerHTML = "Play";
-  }
-}
+  } 
+})
 
 const HOTD = () => {
  window.open("https://xdubteam.blogspot.com/2021/03/high-school-of-dead-hindi-dubbed.html"); 
